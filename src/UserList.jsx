8ae3bf74bffe4cae62cc9659c0eab7153a3c31f0
Aneta590 <anetalisiecka.al@gmail.com>
2021-06-25 10:41:54 +0100

    today

---
+++ src/UserList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+class UsetList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-primary">UserList</h1>
+      </div>
+    );
+  }
+}
+
+export default UserList
